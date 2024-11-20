950965a8c77a8e698541ec736f8aea5457df0334
lvakhoa <22520674@gm.uit.edu.vn>
2024-11-20 08:53:27 +0700

    feat: test

--- src/app/(main-layout)/(protected_routes)/components/HomeCard.tsx
+++ src/app/(main-layout)/(protected_routes)/components/HomeCard.tsx
@@ -14,7 +14,7 @@ function HomeCard({ haveDateRange = false, title, children, date, setDate }: Hom
     <div className='rounded-sm bg-white-100 [box-shadow:0_2px_6px_0_rgba(67,89,113,.12)]'>
       <div className='flex flex-col items-center justify-between border-b border-[rgb(229,231,235)] p-5 sm:flex-row'>
         <h3 className='text-lg font-semibold text-heading'>{title}</h3>
-        {haveDateRange && setDate && <DateRangePicker date={date} setDate={setDate} />}
+        {haveDateRange && <DateRangePicker date={date} setDate={setDate} />}
       </div>
       <div className='p-5'>{children}</div>
     </div>
